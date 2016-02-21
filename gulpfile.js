cb818554c75b050697b7dfb58cca121ecc9ddfec
Shankar Sengupta <shankar.sengupta.dev@gmail.com>
2016-02-21 15:21:45 -0800

    Browserifying store to flux.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var	gulp = require("gulp"),
 	babel = require("gulp-babel"),
 	change = require("gulp-change"),
-	concat = require("gulp-concat");
+	concat = require("gulp-concat"),
+	browserify = require("browserify"),
+	source = require("vinyl-source-stream");
 function normalizeStrict(content){
 	return "'use strict';"+content.replace(/\'use strict\';/g,"");
 }
@@ -9,14 +11,19 @@ function manageReducers(content){
 	return "import {ReducerContainer} from './reducercontainer';\nconst reducerContainer = ReducerContainer.getInstance();\n"+ content + "export default reducerContainer.getReducer();\n";
 		
 }
-gulp.task("compile:reducers",["manage:reducers"], function(){
+gulp.task("flux:dev",["compile store"],function(){
+		return browserify("dist/js/store.js").bundle()
+			.pipe(source("flux.bundle.js"))
+			.pipe(gulp.dest("dist/js"));
+});
+gulp.task("compile reducers",["manage reducers"], function(){
 	return gulp.src(["dev/js/reducer.js","dev/js/reducercontainer.js"])
 		.pipe(babel({
 			presets:['es2015']
 		}))
 		.pipe(gulp.dest('dist/js'));
 });
-gulp.task("compile:store",["compile:reducers"], function(){
+gulp.task("compile store",["compile reducers"], function(){
 	return gulp.src("dev/js/store.js")
 		.pipe(babel({
 			presets:['es2015']
@@ -48,7 +55,7 @@ gulp.task("copy html",function(){
 	return gulp.src('dev/index.html')
 		.pipe(gulp.dest("dist"));
 });
-gulp.task("manage:reducers",function(){
+gulp.task("manage reducers",function(){
 	return gulp.src('dev/js/reducers/*.js')
 		.pipe(concat("reducer.js"))
 		.pipe(change(manageReducers))	
